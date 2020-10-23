1611f5000649ac21d7441570cb28db84e11e72ca
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2020-10-23 20:56:10 +0200

    routing to stores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StoresComponent } from './stores/stores.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/stores', pathMatch: 'full' },
+  { path: 'stores', component: StoresComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
