06de840b7d5bbff39c60142b6afe5f4ba81efce7
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2020-10-23 20:54:37 +0200

    new stores component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoresComponent } from './stores/stores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoresComponent
   ],
   imports: [
     BrowserModule,
