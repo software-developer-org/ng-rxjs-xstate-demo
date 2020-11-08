f186fc3c14aabbdf2ece0e4666414b4fecd1d992
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2020-11-08 22:22:23 +0100

    SoC: separate and extract add log logic

--- src/app/service/log.service.ts
+++ src/app/service/log.service.ts
@@ -12,27 +12,6 @@ export class LogService {
 
   constructor() {}
 
-  log(source: string, ...args: any[]): void {
-    let prefix = getTime() + ' ' + source;
-    while (prefix.length < 40) {
-      prefix += ' ';
-    }
-
-    // concat arguments into one log message
-    const logMessage = prefix + args.reduce((last, current) => {
-      // get stack trace in case of error
-      const stringVal = current?.stack ? current.stack : current;
-      return last + ' ' + stringVal;
-    }, ':');
-
-    // adds a log in a new line
-    this.logs = !this.logs ? logMessage : this.logs + `
-${logMessage}`;
-
-    // multicast logs to Subject
-    this.logs$.next(this.logs);
-  }
-
   getLogs(): Observable<string> {
     return this.logs$;
   }
@@ -40,4 +19,34 @@ ${logMessage}`;
   clear(): void {
     this.logs = null;
   }
+
+  private addLog(source: string, ...args: any[]): void {
+    let prefix = getTime() + ' ' + source;
+    while (prefix.length < 40) {
+      prefix += ' ';
+    }
+
+    // concat arguments into one log message
+    const logMessage =
+      prefix +
+      args.reduce((last, current) => {
+        // get stack trace in case of error
+        const stringVal = current?.stack ? current.stack : current;
+        return last + ' ' + stringVal;
+      }, ':');
+
+    // adds a log in a new line
+    this.logs = !this.logs
+      ? logMessage
+      : this.logs +
+        `
+${logMessage}`;
+  }
+
+  log(source: string, ...args: any[]): void {
+    this.addLog(source, args);
+    // multicast logs to Subject
+    this.logs$.next(this.logs);
+  }
+
 }
